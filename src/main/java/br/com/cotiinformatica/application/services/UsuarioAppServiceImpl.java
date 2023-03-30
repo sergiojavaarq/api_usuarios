f0506fdb9be56f471df5cb11d3342b17ca7f1134
sergiojavaarq <sergio.coti@gmail.com>
2023-03-30 14:25:29 -0300

    Desenvolvimento de producer e testes

--- src/main/java/br/com/cotiinformatica/application/services/UsuarioAppServiceImpl.java
+++ src/main/java/br/com/cotiinformatica/application/services/UsuarioAppServiceImpl.java
@@ -1,22 +1,57 @@
 package br.com.cotiinformatica.application.services;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import br.com.cotiinformatica.application.dtos.CriarContaDTO;
 import br.com.cotiinformatica.application.dtos.CriarContaResponseDTO;
+import br.com.cotiinformatica.application.dtos.EmailMessageDTO;
 import br.com.cotiinformatica.application.interfaces.UsuarioAppService;
 import br.com.cotiinformatica.domain.interfaces.UsuarioDomainService;
+import br.com.cotiinformatica.domain.models.Usuario;
+import br.com.cotiinformatica.infrastructure.producers.MessageProducer;
 
 @Service
 public class UsuarioAppServiceImpl implements UsuarioAppService {
 
 	@Autowired
 	private UsuarioDomainService usuarioDomainService;
-
+	
+	@Autowired
+	private MessageProducer messageProducer;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
 	@Override
 	public CriarContaResponseDTO criarConta(CriarContaDTO dto) {
-		// TODO Auto-generated method stub
-		return null;
+
+		ModelMapper modelMapper = new ModelMapper();
+		
+		Usuario usuario = modelMapper.map(dto, Usuario.class);		
+		usuarioDomainService.criarConta(usuario);
+		
+		CriarContaResponseDTO response = modelMapper.map(usuario, CriarContaResponseDTO.class);
+		response.setMensagem("Conta de usuário criada com sucesso.");
+		
+		//escrevendo a mensagem para incluir na fila
+		EmailMessageDTO emailMessageDTO = new EmailMessageDTO();
+		emailMessageDTO.setTo(usuario.getEmail());
+		emailMessageDTO.setSubject("Parabéns " + usuario.getNome() + ", sua conta foi criada com sucesso!");
+		emailMessageDTO.setBody("Olá, sua conta de usuário foi criada com sucesso em nosso sistema!<br/>Att,<br/>API Usuários");
+		
+		try {
+			//serializando a mensagem e enviando para a fila
+			messageProducer.send(objectMapper.writeValueAsString(emailMessageDTO));
+		}
+		catch(JsonProcessingException ex) {
+			ex.printStackTrace();
+		}
+		
+		return response;
 	}
 }
--- src/test/java/br/com/cotiinformatica/ApiUsuariosApplicationTests.java
+++ src/test/java/br/com/cotiinformatica/ApiUsuariosApplicationTests.java
@@ -1,13 +1,63 @@
 package br.com.cotiinformatica;
 
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Locale;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.javafaker.Faker;
+
+import br.com.cotiinformatica.application.dtos.CriarContaDTO;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ApiUsuariosApplicationTests {
 
+	@Autowired
+	private MockMvc mock;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
 	@Test
-	void contextLoads() {
+	public void atualizarDadosTest() throws Exception {
+		fail("Não implementado.");
+	}
+	
+	@Test
+	public void autenticarTest() throws Exception {
+		fail("Não implementado.");
+	}
+	
+	@Test
+	public void criarContaTest() throws Exception {
+		
+		CriarContaDTO dto = new CriarContaDTO();		
+		Faker faker = new Faker();
+		
+		dto.setNome(faker.name().fullName());
+		dto.setEmail(faker.internet().emailAddress());
+		dto.setSenha("@Teste1234");
+		
+		mock.perform(
+				post("/api/usuarios/criar-conta")
+				.contentType("application/json")
+				.content(objectMapper.writeValueAsString(dto))
+		)
+		.andExpect(status().isCreated());
+	}
+	
+	@Test
+	public void recuperarSenhaTest() throws Exception {
+		fail("Não implementado.");
 	}
 
 }
